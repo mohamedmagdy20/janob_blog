1bde2e2c4758f1749df4d0f1b4ee30bd7675c28a
Eng.Mohamed Maher <75702799+MOMaher891@users.noreply.github.com>
2023-01-20 13:47:54 +0200

    Make Comments as scroller

--- resources/views/front/sections/main.blade.php
+++ resources/views/front/sections/main.blade.php
@@ -47,6 +47,15 @@
             color: red;
         }
 
+        .row {
+            position: relative;
+        }
+
+        .comments {
+            max-height: 170.6px;
+            overflow: scroll;
+        }
+
         .comment {
             position: relative;
         }
@@ -180,119 +189,48 @@
                                 </button>
                             </div>
 
-                            <div class="comment my-3">
-                                <div class="commentBody">
-                                    <p>
-                                        السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
-                                        ههههههههه</p>
-                                </div>
-                            </div>
-                            <div class="comment my-3">
-                                <div class="commentBody">
-                                    <p>
-                                        السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
-                                        ههههههههه</p>
-                                </div>
-                            </div>
-
-
-                            <div class="greyBorder"></div>
-                        </div>
-                        <div class="newsHomePage wow fadeInDown" data-wow-duration="1.4s">
-                            <div class="row">
-                                <a href="categoryNews/0.html">
-                                    <div class="col-6">
-                                        <div class="clean">
-                                            Donate Quran
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="profieInfo">
-                                <a href="shownews/314.html">
-                                    <div class="imgCon">
-                                        <img src="{{ asset('frontCSS/front/images/image.jpg') }}" alt="" />
-                                    </div>
-                                </a>
-                                <div class="infos">
-                                    <div class="date">منذ 4 أيام
-                                    </div>
-
-                                    <div class="name">
-                                        <a style="color: #ff1e1e !important;font-size:24px;" href="shownews/314.html">افضل
-                                            تبرع وصدقة جارية لحفظ كتاب الله تبرع الآن -
-                                            Donate Quran</a>
-                                        <div class="offecial">
-                                            <img src="{{ asset('frontCSS/front/images/check.svg') }}" alt="" />
-                                            رسمي
-                                        </div>
-                                    </div>
-
-                                    <div class="name" style="color:#0d6efd;font-size:12px;">
-                                        كتب: <a href="author-news/1.html">موقع جنوب</a>
+                            <div class="comments">
+                                <div class="comment my-3">
+                                    <div class="commentBody">
+                                        <p>
+                                            السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
+                                            ههههههههه</p>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="newsdes">
-                                <a href="shownews/314.html" style="text-decoration: none">صدقة جارية لدعم تعليم وتحفيظ
-                                    القران
-                                    Ongoing charity to support teaching and memorizing the Qur’an
-                                    You can help lots of people by donating little​ -
-                                    Link below</a>
-                            </div>
-
-                            <a style="text-decoration: none;" href="shownews/314.html">
-                                <img src="{{ asset('frontCSS/images/news/1673847005.jpg') }}" alt=""
-                                    class="bigNewsImg" />
-
-                            </a>
-
-                            <a class="hashTag" href="hashtag-news/55.html">
-                                #quran
-                            </a>
-                            <a class="hashTag" href="hashtag-news/56.html">
-                                #study
-                            </a>
-
-
-
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <div class="numberofcomment">
-                                        <i class="fa-solid fa-comment-dots"></i>
-                                        0
+                                <div class="comment my-3">
+                                    <div class="commentBody">
+                                        <p>
+                                            السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
+                                            ههههههههه</p>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
-                                    <div class="numberoflike">
-                                        <i class="fa-regular fa-heart" onclick="changeLikeIcon()"></i>
-                                        0
+                                <div class="comment my-3">
+                                    <div class="commentBody">
+                                        <p>
+                                            السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
+                                            ههههههههه</p>
                                     </div>
                                 </div>
-                            </div>
-
-
-
-                            <div class="addcomment">
-                                <textarea type="text" name="" id="" class="form-control my-3 commentInput"
-                                    style="min-height: 100%;max-height:500%" placeholder="اكتب تعليقا"></textarea>
-                                <button>
-                                    <i class="fa-solid fa-paper-plane"></i>
-                                </button>
-                            </div>
-
-                            <div class="comment my-3">
-                                <div class="commentBody">
-                                    <p>
-                                        السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
-                                        ههههههههه</p>
+                                <div class="comment my-3">
+                                    <div class="commentBody">
+                                        <p>
+                                            السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
+                                            ههههههههه</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="comment my-3">
-                                <div class="commentBody">
-                                    <p>
-                                        السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
-                                        ههههههههه</p>
+                                <div class="comment my-3">
+                                    <div class="commentBody">
+                                        <p>
+                                            السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
+                                            ههههههههه</p>
+                                    </div>
+                                </div>
+                                <div class="comment my-3">
+                                    <div class="commentBody">
+                                        <p>
+                                            السلام عليكم اخى الكريم احب ان اخبرك بانك مطلوب الى التحقيق من قبل عداله السماء
+                                            ههههههههه</p>
+                                    </div>
                                 </div>
                             </div>
 
